d08d19a7c0cac2a544c9cc3187260ee4a2b1c6cc
jarek-janowski <jarjanowski95@gmail.com>
2021-02-17 15:49:06 +0100

    initial commit

---
+++ src/layouts/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>React Router website</div>
+     );
+  }
+}
+ 
+export default App;
